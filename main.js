ce4272d72c29b1833ca288c14b614b29ea180753
josedasnovinhas <lucas1334pereira@gmail.com>
2024-12-12 19:18:46 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function createGame(player1, hour, player2){
+    return `
+    <li>
+    <img src="assets/times/icon-${player1}.png" alt="Bandeira do ${player1}">
+    <strong>${hour}</strong>
+    <img src="assets/times/icon-${player2}.png" alt="Bandeira do ${player2}">
+    </li>
+    `
+}
+function createCard(date, day, games){
+    return `
+    <div class="card">
+                <h2>${date}<span>${day}</span></h2>
+                <ul>
+                    ${games}
+                </ul>
+            </div>    
+    
+    `
+}
+
+document.getElementById("app").innerHTML = `
+<main id="cards">
+${createCard("11/12", "Quarta-Feira", createGame("atletico", "19:00", "bota"))}
+
+</main>
+`
